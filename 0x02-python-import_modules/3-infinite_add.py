d97cb4007ebe7e568f820ca91cad7aeff1f5456c
OMOLEBILL <105784385+OMOLEBILL@users.noreply.github.com>
2022-09-22 20:20:53 +0200

    modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,17 +1,11 @@
-#!/usr/bin/python3
-def add_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d}".format(n))
-        return
-    else:
-        i = 1
-        add = 0
-        while i <= n:
-            add += int(argv[i])
-            i += 1
-        print("{:d}".format(add))
-
-if __name__ == "__main__":
-    import sys
-    add_arg(sys.argv)
+#!/usr/bin/python3                                                                                                            
+def add_sys(argv):                                                                                                            
+    n = len(argv)                                                                                                             
+    sum = 0                                                                                                                   
+    for i in range(0, n):                                                                                                     
+        if i != 0:                                                                                                            
+            sum += int(argv[i])                                                                                               
+    print("{:d}".format(sum))                                                                                                 
+if __name__ == "__main__":                                                                                                    
+   import sys                                                                                                                 
+   add_sys(sys.argv)
