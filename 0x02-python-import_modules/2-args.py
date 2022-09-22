b630dbf18592ca0dba729d13245be07e64af6618
OMOLEBILL <omolebill01@gmail.com>
2022-09-22 08:47:30 -0700

    modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
     import sys
     n = len(sys.argv)
     if n == 1:
-        print("{:d} arguments.".format(0))
+        print("{:d} arguments.".format(n - 1))
     elif n > 1:
         print("{:d} arguments:".format(n - 1))
         for i in range(0, n):
             if i != 0:
-                print("{:d}: {:s}".format((i), sys.argv[i]))
+                print("{:d}: {:s}".format(i, sys.argv[i]))
