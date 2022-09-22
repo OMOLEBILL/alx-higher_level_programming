730962964a49110333e05f6e83be239bb9403d6e
OMOLEBILL <omolebill01@gmail.com>
2022-09-22 11:51:20 -0700

    modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,14 @@
-#!/usr/bin/python3                                                                                                            
-def add_sys(argv):                                                                                                            
-    n = len(argv)                                                                                                             
-    sum = 0                                                                                                                   
-    for i in range(0, n):                                                                                                     
-        if i != 0:                                                                                                            
-            sum += int(argv[i])                                                                                               
-    print("{:d}".format(sum))                                                                                                 
-if __name__ == "__main__":                                                                                                    
-   import sys                                                                                                                 
-   add_sys(sys.argv)
+#!/usr/bin/python3
+def add_sys(argv):
+
+    n = len(argv)
+    sum = 0
+    for i in range(0, n):
+        if i != 0:
+            sum += int(argv[i])
+    print("{:d}".format(sum))
+
+
+if __name__ == "__main__":
+    import sy
+    add_sys(sys.argv)
