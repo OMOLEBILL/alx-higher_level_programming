1b11d81575aea2e4723b04b79fd10142d268fc49
OMOLEBILL <omolebill01@gmail.com>
2022-09-22 09:47:11 -0700

    modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
-def argx_print(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d} arguments.".format(n))
-        return
-    else:
-        if n == 1:
-            print("{:d} argument:".format(n))
-        else:
-            print("{:d} arguments:".format(n))
-        i = 1
-        while i <= n:
-            print("{:d}: {:s}".format(i, argv[i]))
-            i += 1
 if __name__ == "__main__":
     import sys
-    argx_print(sys.argv)
+    n = len(sys.argv)
+    if n == 1:
+        print("{:d} arguments.".format(n - 1))
+    elif n == 2:
+        print("{:d} argument.".format(n - 1))
+    elif n > 1:
+        print("{:d} arguments:".format(n -1))
+        for i in range(0, n):
+            if i != 0:
+                print("{:d}: {:s}".format(i, sys.argv[i]))
