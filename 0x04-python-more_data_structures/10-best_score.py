4766242e98fa91b5d874ebaa23934f9ff386442e
OMOLEBILL <105784385+OMOLEBILL@users.noreply.github.com>
2022-09-29 02:34:49 +0200

    struct

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     z = max(a_dictionary.values())
-    for keys, value in dictionary.items():
-        if value is z:
-            return keys
+    for keys in a_dictionary.keys():
+            if a_dictionary[keys] == z:
+                b = keys
+     return b
