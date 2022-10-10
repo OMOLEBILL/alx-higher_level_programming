3721fa562b27cc4b9f3170a0b9843cf375064b40
OMOLEBILL <omolebill01@gmail.com>
2022-10-10 12:16:09 -0700

    error

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if str(value).isdigit():
-            print("{:d}".format(value))
-            return True
-        else:
-            return False
-        except ValueError:
-            print("no such thing")
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        return False
