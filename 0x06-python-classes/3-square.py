e3999855f18cc4aaa35116633c5e927c695a1079
OMOLEBILL <omolebill01@gmail.com>
2022-10-11 14:43:10 -0700

    oop

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+class Sqaure qith a private instance attribute siz
+"""
+
+
+class Square:
+    """
+    Class Square with a private instance attribute size
+    """
+    def __init__(self, size=0):
+        """ args:
+                 size
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    """
+    Returns area
+    """
+
+    def area(self):
+        """
+        Return the area of the sqaure
+        """
+        return (self.__size * self.__size)
