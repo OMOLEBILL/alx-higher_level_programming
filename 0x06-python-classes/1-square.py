7b3f8ec656528bf7716ecf0067dcf4a2a0cffa3a
OMOLEBILL <omolebill01@gmail.com>
2022-10-11 14:12:41 -0700

    oop

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 
+"""
+class Sqaure qith a private instance attribute siz
+"""
+
 
 class Square:
     """
     Class Square with a private instance attribute size
     """
     def __init__(self, size):
+        """ args:
+                 size
+        """
         self.__size = size
