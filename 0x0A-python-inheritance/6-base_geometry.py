005f7d46356a8608f156856341ffefba0794c775
OMOLEBILL <omolebill01@gmail.com>
2022-10-24 07:30:27 -0700

    inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Raises an exception
+"""
+
+
+class BaseGeometry():
+    """ Raises ana exception
+    """
+    def area(self):
+        """raises an exception
+        """
+        raise Exception("area() is not implemented")
