c0ed28a109c10773a2a7d3f6a20b1139aea46925
OMOLEBILL <omolebill01@gmail.com>
2022-10-24 06:17:02 -0700

    inheritance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Checking the exact istances
+"""
+
+
+def is_same_class(obj, a_class):
+    """check if an obj is an istance of a class
+    """
+    if type(obj) is a_class:
+        return True
+    return False
