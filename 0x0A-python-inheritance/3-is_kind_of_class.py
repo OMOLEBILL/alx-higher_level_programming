a1e441c6deadbe10514d810886d6c3e62667df87
OMOLEBILL <omolebill01@gmail.com>
2022-10-24 06:22:38 -0700

    inheritance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Checks if is kind of
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Checks if is kind of
+    """
+    if isinstance(obj, a_class):
+        return False
+    return True
