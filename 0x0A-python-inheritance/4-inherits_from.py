ef1ea821e109bc4b3d402d5eb8370c01e3a903c1
OMOLEBILL <omolebill01@gmail.com>
2022-10-24 06:46:00 -0700

    inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,6 +6,6 @@
 def inherits_from(obj, a_class):
     """checks the inheritance of a class
     """
-    if isinstance(obj, a_class) and type(obj) is a_class:
-        return False
-    return True
+    if type(obj) != a_class and isinstance(obj, a_class):
+        return True
+    return False
