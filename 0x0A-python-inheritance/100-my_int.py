7d5273d9626f241c88aad43bafbfeab3a0cff26e
OMOLEBILL <omolebill01@gmail.com>
2022-10-24 13:59:53 -0700

    inheritance

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,17 +6,12 @@
 class MyInt(int):
     """class MyInt
     """
-    def __init__(self, value):
-        """initilaizes value
-        """
-        self.name = value
-
     def __eq__(self, other):
         """eq
         """
-        return self.num != other
+        return super().__ne__(other)
 
     def __ne__(self, other):
         """ne
         """
-        return self.num == other
+        return super().__eq__(other)
