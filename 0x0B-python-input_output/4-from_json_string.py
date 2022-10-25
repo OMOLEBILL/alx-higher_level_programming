60cff88ecaf04a2dcdf2cfbd1e72c8c288c242a3
OMOLEBILL <omolebill01@gmail.com>
2022-10-25 13:21:21 -0700

    io

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""decoding a json file
+"""
+
+
+import json
+"""the json module
+"""
+
+
+def from_json_string(my_str):
+    """decoding json
+    """
+    return (json.loads(my_str))
