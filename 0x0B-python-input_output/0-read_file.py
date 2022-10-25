155fce56e4795d9343771ba6b589cf8e35a26c30
OMOLEBILL <omolebill01@gmail.com>
2022-10-25 12:16:42 -0700

    io

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Reads froma file
+"""
+
 
 def read_file(filename=""):
+    """reads froma a file
+    """
     with open(filename, 'r',  encoding='utf-8') as f:
         print(f.read(), end="")
