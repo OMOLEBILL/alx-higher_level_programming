b6eee51f619e7afebddf3cc406cb528fa801796f
OMOLEBILL <omolebill01@gmail.com>
2022-10-25 15:02:52 -0700

    io

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""class student
+"""
+
+
+class Student():
+    """class student
+    """
+    def __init__(self, first_name, last_name, age):
+        """instanciate the class
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """dict of class
+        """
+        z = {}
+        if type(attrs) is list and all(type(x) for x in attrs):
+            for i in attrs:
+                if i in self.__dict__:
+                    z.update({i: self.__dict__[i]})
+            return z
+        return self.__dict__
