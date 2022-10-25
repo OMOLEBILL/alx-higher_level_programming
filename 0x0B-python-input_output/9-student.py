e2a386e1829ced701d6ca7cf34ee6185686879dc
OMOLEBILL <omolebill01@gmail.com>
2022-10-25 14:40:17 -0700

    io

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""class student
+"""
+
+
+class Student():
+    """class student
+    """
+    def __init__(self, first_name, last_name, age):
+        """instanciate the class
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = last_name
+
+    def to_json(self):
+        """dict of class
+        """
+        return self.__dict__
