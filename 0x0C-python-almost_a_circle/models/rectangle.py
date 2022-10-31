ef63ce66e6bd95581f489c8777f053fed8a27728
OMOLEBILL <omolebill01@gmail.com>
2022-10-31 10:52:09 -0700

    almost

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+from base import Base
+"""We get the base class from the modules base
+"""
+
+
+class Rectangle(Base):
+    """Rectangle class
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """we initialize the the class
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """ width
+        """
+        return (self.__width)
+
+    @width.setter
+    def width(self, width):
+        """ set the value of width
+        """
+        self.__width = width
+
+    @property
+    def height(self):
+        """ return the value of height
+        """
+        return (self.__height)
+
+    @height.setter
+    def height(self, height):
+        """ sets the value of height
+        """
+        self.__height = height
+
+    @property
+    def x(self):
+        """ return the value of x
+        """
+        return (self.__x)
+
+    @x.setter
+    def x(self, x):
+        """ sets the value of x
+        """
+        self.__x = x
+
+    @property
+    def y(self):
+        """ returns the value of y
+        """
+        return (self.__y)
+
+    @y.setter
+    def y(self, y):
+        """ sets the value for y
+        """
+        self.__y = y
