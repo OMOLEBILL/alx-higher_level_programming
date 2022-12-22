f9661bed364add516f9c268888aa20ccbf696844
OMOLEBILL <omolebill01@gmail.com>
2022-12-22 14:14:52 +0000

    session sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""list all states using sqlalchemy"""
+
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.format(
+                argv[1], argv[2], argv[3]))
+    Session = sessionmaker(engine)
+    with Session() as session:
+        for state in session.query(State).order_by(State.id):
+            print(f"{state.id}: {state.name}")
