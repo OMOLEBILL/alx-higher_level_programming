6d046f8c0830689d36317387e7a26d908dc7cb50
OMOLEBILL <omolebill01@gmail.com>
2022-12-22 19:05:29 +0000

    all

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""list all states using sqlalchemy"""
-
-from model_state import Base, State
+"""
+All states via SQLAlchemy
+"""
 from sys import argv
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import Session
 
-
 if __name__ == "__main__":
-    engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost/{}'.format(
-                argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
+
     session = Session(engine)
-    c1 = session.query(State).filter(State.name.like("%"))\
-                .delete(synchronize_session='fetch')
+    data = session.query(State).filter(State.name.like("%a%"))\
+                               .delete(synchronize_session='fetch')
     session.commit()
     session.close()
