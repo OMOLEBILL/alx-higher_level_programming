5101fb03fe35b1f1b044daa890d9e8ed7cf05486
OMOLEBILL <omolebill01@gmail.com>
2022-12-22 08:22:54 +0000

    filter by BINARY N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" this scripts list the states name"""
+
+
+import MySQLdb
+from sys import argv
+
+""" listing states from a data base """
+
+if __name__ == "__main__":
+    con = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                          password=argv[2], database=argv[3])
+    cur = con.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for rows in query_rows:
+        print(rows)
+    cur.close()
+    con.close()
