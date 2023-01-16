dac637c7886f663a7ad14721a3d04194f13624fc
OMOLEBILL <omolebill01@gmail.com>
2023-01-16 15:57:44 +0000

    delete

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 #send a delete request
-# Get the URL from the first command line argument
-url=$1
-
 # Send the DELETE request to the URL using curl
-curl -s -X DELETE "$url"
+curl -s -X DELETE "$1"
