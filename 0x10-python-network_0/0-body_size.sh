e3dbfd69fc5788be242659b5fbf7b3ae2e8db177
OMOLEBILL <omolebill01@gmail.com>
2023-01-16 15:44:28 +0000

    curl

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 #check content length
-
-# Get the URL from the first command line argument
-url=$1
-
-# Send the request to the URL using curl
-response=$(curl -s -D - "$url" -o /dev/null)
-
-# Extract the size of the body from the response headers
-size=$(echo "$response" | grep -Fi Content-Length | awk '{print $2}')
-
-# Print the size of the body in bytes
-echo "$size"
+curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
