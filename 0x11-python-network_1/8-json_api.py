dee55564e1786fecce4107d8e384e847f82b0c0b
OMOLEBILL <omolebill01@gmail.com>
2023-01-17 23:27:29 +0000

    connection

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,14 +5,15 @@
 if __name__ == "__main__":
     import requests
     from sys import argv
+    url = "http://0.0.0.0:5000/search_user"
 
-    q = argv[1] if len(argv) > 1 else ""
-    data = requests.post(argv[1], json={"q": q})
+    q = argv[1] if len(argv) == 2 else ""
+    data = requests.post(url, json={"q": q})
     try:
         cont = data.json()
         if not cont:
             print("No result")
         else:
-            print("[{}] {}".format(cont["id"], cont["name"]))
-    except ValueError:
+            print("[{}] {}".format(cont.get("id"), cont.get("name")))
+    except:
         print("Not a valid JSON")
