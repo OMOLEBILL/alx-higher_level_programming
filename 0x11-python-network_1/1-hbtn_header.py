c9177ea08016604b50470b60bb07152ec18814af
OMOLEBILL <omolebill01@gmail.com>
2023-01-17 19:13:50 +0000

    noimports:

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,10 +3,10 @@ import urllib.request
 import sys
 """this module gets a specific value from the entered url"""
 
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-url = sys.argv[1]
-
-with urllib.request.urlopen(url) as response:
-    headers = response.info()
-    x_request_id = headers.get("X-Request-Id")
-    print(x_request_id)
+    with urllib.request.urlopen(url) as response:
+        headers = response.info()
+        x_request_id = headers.get("X-Request-Id")
+        print(x_request_id)
