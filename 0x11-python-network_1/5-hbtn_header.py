f7d82d535a6d92418c23a028e57445d294a009ce
OMOLEBILL <omolebill01@gmail.com>
2023-01-17 21:17:30 +0000

    update

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,8 +4,8 @@
 
 if __name__ == "__main__":
     import requests
-    from sys import argv
+    import sys
 
-    data = requests.get(argv[1])
+    data = requests.get(sys.argv[1])
     if data.status_code == 200:
         print(data.headers["X-Request-Id"])
