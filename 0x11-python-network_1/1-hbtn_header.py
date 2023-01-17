8772e36f2e61200a252889b23158af9783b68da2
OMOLEBILL <omolebill01@gmail.com>
2023-01-17 19:18:53 +0000

    maybe.info()

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-import urllib.request
-import sys
 """this module gets a specific value from the entered url"""
 
 if __name__ == "__main__":
+    import urllib.request
+    import sys
     url = sys.argv[1]
 
     with urllib.request.urlopen(url) as response:
-        headers = response.info()
-        x_request_id = headers.get("X-Request-Id")
+        x_request_id = response.headers.get("X-Request-Id")
         print(x_request_id)
